b4d4b729f5d4a2afaff068d3a55e8ac889a45056
Anh Minh Tu <anhminh.tu@student.uts.edu.au>
2024-01-22 11:31:07 +1100

    Defining project structure

--- scripts/test01.py
+++ scripts/test01.py
@@ -4,5 +4,13 @@ from spatialmath import SE3
 from spatialmath.base import argcheck
 from spatialmath import base
 
+from swift import Swift
+
 # Create a PR2 robot object
 pr2 = rtb.models.PR2()
+
+env = Swift()
+env.launch()  
+env.add(pr2)
+  
+env.hold()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=["bimanual_controller"],
+    package_dir={"": "src"},
+    install_requires=["numpy", "scipy", "spatialmath-python", "roboticstoolbox-python"],
+)
+
+setup(**d)
