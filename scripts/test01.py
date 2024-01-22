fa2866b51dfa93dbc016f98b706832d4d7a7ddcc
Anh Minh Tu <anhminh.tu@student.uts.edu.au>
2024-01-22 11:05:17 +1100

    Initial commit with ROS package

---
+++ scripts/test01.py
@@ -0,0 +1,8 @@
+import numpy
+import roboticstoolbox as rtb
+from spatialmath import SE3
+from spatialmath.base import argcheck
+from spatialmath import base
+
+# Create a PR2 robot object
+pr2 = rtb.models.PR2()
