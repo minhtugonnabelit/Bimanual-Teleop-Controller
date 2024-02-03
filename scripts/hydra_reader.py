dd3fdd15d765a0fa8483e6b17951139cd7d0ce68
AnhMinhTu <AnhMinh.Tu@student.uts.edu.au>
2024-02-03 13:31:41 +1100

    Fix conflict

--- scripts/hydra_reader.py
+++ scripts/hydra_reader.py
@@ -2,4 +2,23 @@ import rospy
 import tf
 import spatialmath.base as smb
 import numpy as np
+from geometry_msgs.msg import PoseStamped, TwistStamped
+
+class HydraReader:
+
+    def __init__(self):
+        rospy.init_node('hydra_reader', anonymous=True)
+        self.listener = tf.TransformListener()
+        self.pose_pub = rospy.Publisher('/hydra_pose', PoseStamped, queue_size=1)
+        self.twist_pub = rospy.Publisher('/hydra_twist', TwistStamped, queue_size=1)
+        self.rate = rospy.Rate(100)
+
+    def pose_callback(self):
+
+        pass
+
+
+    def twist_conversion(self):
+        
+        pass
 
